913ba16e8aa26aa6ec7d53ba8f68e1a31548b3b6
aalejo92 <76714792+aalejo92@users.noreply.github.com>
2020-12-30 11:07:22 -0500

    Add files via upload

---
+++ Rock, Paper, Scissors.py
@@ -0,0 +1,17 @@
+import random
+def play():
+    user = input("Choose 'r' for rock, 'p' for paper, 's' for scissors ")
+    computer = random.choice(['r', 'p', 's'])
+    if user == computer:
+        return "It's a Tie"
+    if winner(user, computer):
+        return 'You Win'
+    else:
+        return 'You Lose'
+
+def winner(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 'p' and opponent == 'r') \
+            or (player == 's' and opponent == 'p'):
+        return True
+
+print(play())
