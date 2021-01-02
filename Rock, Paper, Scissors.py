e482b0d5fe724347bf0321ceed36c0a0b5c47600
aalejo92 <76714792+aalejo92@users.noreply.github.com>
2021-01-02 11:42:01 -0500

    Update Rock, Paper, Scissors.py

--- Rock, Paper, Scissors.py
+++ Rock, Paper, Scissors.py
@@ -1,17 +1,21 @@
 import random
+
+
 def play():
     user = input("Choose 'r' for rock, 'p' for paper, 's' for scissors ")
     computer = random.choice(['r', 'p', 's'])
     if user == computer:
-        return "It's a Tie"
+        return "It's a Tie!"
     if winner(user, computer):
-        return 'You Win'
+        return 'You Win!'
     else:
-        return 'You Lose'
+        return 'You Lose!'
+
 
 def winner(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 'p' and opponent == 'r') \
             or (player == 's' and opponent == 'p'):
         return True
+
 
 print(play())
